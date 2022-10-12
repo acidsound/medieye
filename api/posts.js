e0948ee1ecab1e52584930daf9c666a49bf72255
LEE JAE HO <spectrick@gmail.com>
2022-10-12 04:53:40 +0000

    type: module + import 사용, axios는 최신화

--- api/posts.js
+++ api/posts.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+import axios from 'axios';
 export default async (request, response) => {
     const result = await axios("https://jsonplaceholder.typicode.com/posts");
     response.status(200).end(JSON.stringify(result.data));
